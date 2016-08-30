2c3d214c701124ca98d473c164d1904d811902e4
ladinmbdn@gmail.com <ladinmbdn@gmail.com>
2016-08-30 18:11:05 +0800

    多线程

---
+++ hexin/jincheng2.py
@@ -0,0 +1,22 @@
+import thread
+from time import sleep, ctime
+
+def loop0():
+    print 'start loop 0 at:', ctime()
+    sleep(4)                            #暂停4秒
+    print 'loop 0 done at:', ctime()
+
+def loop1():
+    print 'start loop ` at:', ctime()
+    sleep(2)
+    print 'loop 1 done at:', ctime()
+
+def main():
+    print 'starting at:', ctime()
+    thread.start_new_thread(loop0,())
+    thread.start_new_thread(loop1,())
+    sleep(6)
+    print 'all DONE at: ', ctime()
+
+if __name__ == '__main__':
+    main()
